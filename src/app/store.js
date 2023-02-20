c7fb06258c659691a2dd6d807689bb27a58fa765
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-02-20 11:46:43 -0600

    Remove files to start over on redux understanding

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import booksReducer from '../features/books/booksSlice'
 
 export const store = configureStore({
   reducer: {
-    books: booksReducer
+    
   }
 });
