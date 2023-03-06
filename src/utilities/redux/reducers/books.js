6c3c53d44f8e19f3c7c2d040b484deda0babb946
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-06 11:00:25 -0600

    Create reducers and actions

---
+++ src/utilities/redux/reducers/books.js
@@ -0,0 +1,14 @@
+const initialState = {
+    booksData: {}
+}
+
+const books = (state = initialState, action) => {
+    switch (action.type) {
+        case "setBooksData":
+            return {...state, booksData: action.booksData }
+        default:
+            return state
+    }
+}
+
+export default books
