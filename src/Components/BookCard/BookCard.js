053d1e5aee00ec32b8998e30353bb293bc9befe4
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-06 15:23:48 -0600

    Stylize booksbox

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -2,6 +2,14 @@ import './BookCard.css'
 
 export const BookCard = () => {
     return (
-        <p className='title'></p>
+        <div>
+            <img 
+                className='card-img'
+                src={ book.image }
+                alt={`thumbnail of ${ book.title }`}
+            />
+            <h3 className='title'>{ book.title }</h3>
+            <p>{ book.author }</p>
+        </div>
     )
 }
--- src/Components/BooksBox/BooksBox.js
+++ src/Components/BooksBox/BooksBox.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import { getData } from '../../utilities/apiCalls'
+
+import './BooksBox.css'
  
 export const BooksBox = () => {
 
@@ -17,14 +19,20 @@ export const BooksBox = () => {
     )
 
     return (
-        <section>
+        <section className='book-container'>
             { books
                 .filter((book) => 
                     filterBy ? book.title.includes(filterBy) : true 
                 )
                 .map((book) => (
-                    <div key={book.title}>
-                        {book.title}
+                    <div key={book.title} className='book-card'>
+                        <img 
+                            className='card-img'
+                            src={ book.book_image }
+                            alt={`thumbnail of ${ book.title }`}
+                        />
+                        <h3 className='title'>{ book.title }</h3>
+                        <p>{ book.author }</p>
                     </div>
                 ))}
         </section>
